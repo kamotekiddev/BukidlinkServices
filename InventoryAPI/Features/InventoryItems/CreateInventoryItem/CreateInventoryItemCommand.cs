16c72838f64bbfc0111489a53bb759b80144800e
kamotekid <kamotekiddev@gmail.com>
2026-04-16 12:16:41 +0800

    remove solution folder

--- InventoryAPI/Features/InventoryItems/CreateInventoryItem/CreateInventoryItemCommand.cs
+++ InventoryAPI/Features/InventoryItems/CreateInventoryItem/CreateInventoryItemCommand.cs
@@ -3,4 +3,5 @@ using MediatR;
 
 namespace InventoryAPI.Features.InventoryItems.CreateInventoryItem;
 
-public record CreateInventoryItemCommand(Guid ProductVariantId, int Quantity, int ReservedQuantity) : IRequest<InventoryItem>;
+public record CreateInventoryItemCommand(Guid ProductVariantId, int Quantity, int ReservedQuantity)
+    : IRequest<InventoryItem>;
--- ProductCatalogAPI/Common/Errors/ProblemDetailsFactory.cs
+++ ProductCatalogAPI/Common/Errors/ProblemDetailsFactory.cs
@@ -30,6 +30,4 @@ public abstract class ProblemDetailsFactory
             Title = "Validation failed",
         };
     }
-
-
 }
